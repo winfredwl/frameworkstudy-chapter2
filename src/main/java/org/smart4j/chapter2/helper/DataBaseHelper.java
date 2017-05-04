aee8983cf431b65790b126066c80fce33ac22c27
wanglei <wangl1038@joyowo.com>
2017-05-04 11:06:27 +0800

    使用数据库连接池

--- src/main/java/org/smart4j/chapter2/helper/DataBaseHelper.java
+++ src/main/java/org/smart4j/chapter2/helper/DataBaseHelper.java
@@ -1,5 +1,6 @@
 package org.smart4j.chapter2.helper;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
@@ -25,27 +26,28 @@ import java.util.Properties;
 public final class DataBaseHelper {
     private static final Logger logger = LoggerFactory.getLogger(DataBaseHelper.class);
 
-    private static final ThreadLocal<Connection> CONNECTION_HOLDER = new ThreadLocal<>();
+    private static final ThreadLocal<Connection> CONNECTION_HOLDER;
 
-    private static final QueryRunner QUERY_RUNNER = new QueryRunner();
+    private static final QueryRunner QUERY_RUNNER;
 
-    private static final String DRIVER;
-    private static final String URL;
-    private static final String USERNAME;
-    private static final String PASSWORD;
+    private static final BasicDataSource DATA_SOURCE;
 
     static {
-        Properties conf = PropertiesUtil.loadProps("config.properties");
-        DRIVER = conf.getProperty("jdbc.driver");
-        URL = conf.getProperty("jdbc.url");
-        USERNAME = conf.getProperty("jdbc.username");
-        PASSWORD = conf.getProperty("jdbc.password");
+        CONNECTION_HOLDER = new ThreadLocal<>();
 
-        try {
-            Class.forName(DRIVER);
-        } catch (ClassNotFoundException e) {
-            logger.error("can not load jdbc driver", e);
-        }
+        QUERY_RUNNER = new QueryRunner();
+
+        Properties conf = PropertiesUtil.loadProps("config.properties");
+        String driver = conf.getProperty("jdbc.driver");
+        String url = conf.getProperty("jdbc.url");
+        String username = conf.getProperty("jdbc.username");
+        String password = conf.getProperty("jdbc.password");
+
+        DATA_SOURCE = new BasicDataSource();
+        DATA_SOURCE.setDriverClassName(driver);
+        DATA_SOURCE.setUrl(url);
+        DATA_SOURCE.setUsername(username);
+        DATA_SOURCE.setPassword(password);
     }
 
     /**
@@ -55,7 +57,7 @@ public final class DataBaseHelper {
         Connection conn = CONNECTION_HOLDER.get();
         if (conn == null) {
             try {
-                conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                conn = DATA_SOURCE.getConnection();
             } catch (SQLException e) {
                 logger.error("get connection failure", e);
                 throw new RuntimeException(e);
@@ -64,23 +66,6 @@ public final class DataBaseHelper {
             }
         }
         return conn;
-    }
-
-    /**
-     * 关闭数据库连接
-     */
-    public static void closeConnection() {
-        Connection conn = CONNECTION_HOLDER.get();
-        if (conn != null) {
-            try {
-                conn.close();
-            } catch (SQLException e) {
-                logger.error("close connection failure", e);
-                throw new RuntimeException(e);
-            } finally {
-                CONNECTION_HOLDER.remove();
-            }
-        }
     }
 
     /**
@@ -94,8 +79,6 @@ public final class DataBaseHelper {
         } catch (SQLException e) {
             logger.error("query entity list failure", e);
             throw new RuntimeException(e);
-        } finally {
-            closeConnection();
         }
         return entityList;
     }
@@ -111,8 +94,6 @@ public final class DataBaseHelper {
         } catch (SQLException e) {
             logger.error("query entity failure", e);
             throw new RuntimeException(e);
-        } finally {
-            closeConnection();
         }
         return entity;
     }
@@ -143,8 +124,6 @@ public final class DataBaseHelper {
         } catch (SQLException e) {
             logger.error("execute update failure", e);
             throw new RuntimeException(e);
-        } finally {
-            closeConnection();
         }
         return rows;
     }
